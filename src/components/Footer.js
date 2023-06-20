f5f6f5cfbf89ea7561314b9032ebe280031a7af8
pravalika attada <pravalikaattada15@gmail.com>
2023-06-20 14:13:20 +0530

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-gray-900 text-white py-6">
       <div className="container mx-auto px-4 text-center">
         <p className="text-lg">
-          © {new Date().getFullYear()} Your Name. All rights reserved.
+          © {new Date().getFullYear()} Pravalika Attada. All rights reserved.
         </p>
       </div>
     </footer>
